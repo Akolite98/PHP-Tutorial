6199be2e2b1d92596a8ed438c5ec6811d5fb319e
akolite <nnorompreciousakachukwu@gmail.com>
2021-01-28 17:32:26 +0100

    Modified an existing file

--- type_casting.php
+++ type_casting.php
@@ -8,6 +8,28 @@
 <body>
 
 type juggling <br>
+
+<?php $count = "2 cats";?>
+type: <?php echo gettype($count);?><br>
+
+<?php $count += 3; ?>
+type: <?php echo gettype($count);?><br>
+
+<?php $cats = "I have " .  $count .  " cats."?>
+<!-- <?php echo $cats;?><br> -->
+cats:<?php echo gettype($cats);?>
+<br>
+
+type casting <br>
+<?php settype($count, "integer"); ?>
+count: <?php echo gettype($count);?><br>
+
+<?php $count2 = (string)  $count;?>
+count: <?php echo gettype($count);?><br>
+count2: <?php echo gettype($count2);?><br>
+
+
+
     
 </body>
 </html>
