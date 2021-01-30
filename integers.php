92a2691f11ca959a96ab57740750a17dd0c8cf5c
akolite <nnorompreciousakachukwu@gmail.com>
2021-01-30 14:26:58 +0100

    added some new files

---
+++ integers.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Intergers</title>
+</head>
+<body>
+
+
+    <?php
+      $var1 = 3;
+      $var2 = 4;
+    ?>
+    Basic Math: <?php echo ((1 + 2 + $var1) * $var2) / 2 - 5; ?><br />
+    <br />
+    Absolute value:   <?php echo abs(0 - 300);   ?><br />
+    Exponential:       <?php echo pow(2,8);       ?><br />
+    Square root:       <?php echo sqrt(100);     ?><br />
+    Modulo:           <?php echo fmod(20,7);     ?><br />
+    Random:           <?php echo rand();         ?><br />
+    Random (min,max): <?php echo rand(1,10);     ?><br />
+    <br />
+    += : <?php $var2 += 4; echo $var2; ?><br />
+    -= : <?php $var2 -= 4; echo $var2; ?><br />
+    *= : <?php $var2 *= 3; echo $var2; ?><br />
+    /= : <?php $var2 /= 4; echo $var2; ?><br />
+    <br />
+    Increment: <?php $var2++; echo $var2; ?><br />
+    Decrement: <?php $var2--; echo $var2; ?><br />
+    <br />
+    <?php
+      // PHP will convert a string to an integer
+      // but it is sloppy programming
+      echo 1 + "2 houses";
+    ?>
+
+  
+</body>
+</html>
---
+++ whileloops.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Loops: while</title>
+</head>
+<body>
+
+
+    <?php
+      $count = 0;
+      while ($count <= 10) {
+        if ($count == 5) {
+          echo "FIVE, ";
+        } else {
+          echo $count . ", ";
+        }
+        $count++;  // increment by 1
+      }
+      echo "<br />";
+      echo "Count: {$count}";
+    ?>
+
+    <br />
+    <br />
+    <?php // On your own exercise
+    
+      $count = 1;
+      while ($count < 20) {
+        if($count % 2 == 0) {
+          echo "{$count} is even<br />";
+        } else {
+          echo "{$count} is odd<br />";
+        }
+        $count++;
+      }
+    
+    ?>
+
+
+  
+</body>
+</html>
