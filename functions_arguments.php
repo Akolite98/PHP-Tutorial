5ed162363b3b01bac580aa6e89fb6f7a295ff95c
akolite <nnorompreciousakachukwu@gmail.com>
2021-02-01 15:27:51 +0100

    New files

---
+++ functions_arguments.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Functions: Arguments</title>
+</head>
+<body>
+
+
+    <?php
+
+      function say_hello_to($word) {
+        echo "Hello {$word}!<br />";
+      }
+
+      $name = "John Doe";
+      say_hello_to($name);
+
+    ?>
+    
+    <?php
+    
+      function better_hello($greeting, $target, $punct) {
+        echo $greeting . " " . $target . $punct . "<br />";
+      }
+    
+      better_hello("Hello", $name, "!");
+      better_hello("Greetings", $name, "!!!");
+
+      better_hello("Greetings", $name, null);
+    
+    ?>
+
+  
+
+  
+</body>
+</html>
---
+++ functions_defaults.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Functions: Default Argument Values</title>
+</head>
+<body>
+
+
+    <?php
+    
+      function paint($room="office",$color="red") {
+        return "The color of the {$room} is {$color}.<br />";
+      }
+    
+      echo paint();
+      echo paint("bedroom", "blue");
+      echo paint("bedroom", null);
+      echo paint("bedroom");
+      echo paint("blue");
+    
+  
+    ?>
+
+
+
+ 
+  
+  
+</body>
+</html>
